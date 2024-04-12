d5c63653dd71a373b66047506cc6ee4b289dd0f2
Prot0ast <a_barragan1@u.pacific.edu>
2024-04-12 09:42:27 -0500

    Setting up the home page layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,19 @@ import React from "react";
 import logo from "./logo.svg";
 import { ThemeContext } from "./ThemeContext";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HomePage } from "./pages/HomePage";
 import "./App.css";
 
 function App() {
-  return <ThemeContext.Provider value="dark"></ThemeContext.Provider>;
+  return (
+    <ThemeContext.Provider value="dark">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeContext.Provider>
+  );
 }
 
 export default App;
---
+++ src/components/Footer.component.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+  return (
+    <footer>
+      <p>Copyright 2024 Quantum Com *not a real company*</p>
+    </footer>
+  );
+}
