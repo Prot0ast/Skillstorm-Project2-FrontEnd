c4313504d6495d3bff001f176994b03d5ec6562b
Prot0ast <a_barragan1@u.pacific.edu>
2024-04-16 10:45:09 -0500

    I'm cooking

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -35,6 +35,44 @@ export function HomePage() {
         <div className="background">:)</div>
         <a className="btn-btn-primary btn-lg" href="/customers">View Customers</a>
       </div>
+      <body>
+        <br />
+        <h4 className="centerText">New User? Register Here!</h4>
+        <form action="https://localhost:5001/api" method="post">
+            <div className="container">
+              <label htmlFor="firstName">First Name:</label>
+              <input type="text" className="form-control" id="firstName" name="firstName" placeholder="Enter First Name here..." required/>
+            </div>
+            <div className="container">
+              <label htmlFor="lastName">Last Name:</label>
+              <input type="text" className="form-control" id="lastName" name="lastName" placeholder="Enter Last Name here..." required/>
+            </div>
+            <div className="container">
+              <label htmlFor="userName">Username: </label>
+              <input type="text" className="form-control" id="userName" name="userName" placeholder="Enter Username here..." required/>
+            </div>
+            <div className="container">
+              <label htmlFor="password">Password: </label>
+              <h6>Must be 10 characters long</h6>
+              <h6></h6>
+              <input type="text" className="form-control" id="password" name="password" placeholder="Enter Password here..." pattern=".{10,}" required/>
+            </div>
+            <div className="container">
+              <label htmlFor="email">Email: </label>
+              <input type="text" className="form-control" id="email" name="email" placeholder="Enter Email here..." required/>
+            </div>
+            <div className="container">
+              <label htmlFor="phoneNumber">Phone Number: </label>
+              <h6>Format: ###-###-####</h6>
+              <input type="tel" className="form-control" id="phoneNumber" name="phoneNumber" placeholder="Enter Phone Number here..." pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required/>
+            </div>
+            <div className="centerText">
+              <br />
+              <button className="btn btn-success">Submit</button>
+            </div>
+        </form>
+
+      </body>
       <Footer />
     </>
   );
