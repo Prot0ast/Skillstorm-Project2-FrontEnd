3150bbf972ae72a89cf8a38708a848cebab78317
Prot0ast <a_barragan1@u.pacific.edu>
2024-04-16 09:06:53 -0500

    Centering the properties via CSS

--- src/components/Footer.component.tsx
+++ src/components/Footer.component.tsx
@@ -1,3 +1,5 @@
+import "./Component.css";
+
 export function Footer() {
   return (
     <footer>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,6 +3,7 @@ import { ThemeContext } from "../ThemeContext";
 import { Footer } from "../components/Footer.component";
 import { Header } from "../components/Header.component";
 import './Pallete.css';
+import './Page.css';
 
 export function HomePage() {
   const [count, setCount] = React.useState(0);
@@ -25,9 +26,10 @@ export function HomePage() {
   return (
     <>
       <Header />
-      <div>
-        <img src="QuantumCom.png" alt="QuantumCom logo" />
+      <div className="centerText">
+        <img className="centerImg" src="QuantumCom.png" alt="QuantumCom logo" />
         <br />
+        <div className="background">:)</div>
         <a className="btn-btn-primary btn-lg" href="/customers">View Customers</a>
       </div>
       <Footer />
